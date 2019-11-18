6fc63a8476f099e7a55a64dbb5f13e522a701156
David Crenin <david.crenin@gmail.com>
2019-11-18 11:42:39 +0100

    Start a new a Django app

---
+++ odile_project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for odile_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "odile_project.settings")
+
+application = get_wsgi_application()
