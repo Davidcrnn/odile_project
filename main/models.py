e36408c424c9479be42630bce474dbf824ba390c
David Crenin <david.crenin@gmail.com>
2019-11-21 08:21:08 +0100

    Order summary and Checkout process done

--- main/models.py
+++ main/models.py
@@ -19,13 +19,22 @@ class Product(models.Model):
     def get_add_to_cart_url(self):
         return reverse('add-to-cart', kwargs={'slug': self.slug})
 
+    def get_remove_from_cart_url(self):
+        return reverse('remove-from-cart', kwargs={'slug': self.slug})
+
 
 class OrderProduct(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
         return f"{self.quantity} of {self.product.name}"
+
+    def get_total_product_price(self):
+        return self.quantity * self.product.price
 
 
 class Order(models.Model):
@@ -36,3 +45,9 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def get_total(self):
+        total = 0
+        for order_product in self.products.all():
+            total += order_product.get_total_product_price()
+        return total
--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import HomePageView, ProductListView, ProductDetailView, add_to_cart
+from .views import HomePageView, ProductListView, ProductDetailView, add_to_cart, remove_from_cart, OrderSummaryView, remove_single_product_from_cart, CheckoutView, DeliveryView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('products/', ProductListView.as_view(), name='products'),
     path('products/<slug>', ProductDetailView.as_view(), name='product-detail'),
-    path('add_to_cart/<slug>/', add_to_cart, name='add-to-cart')
+    path('add_to_cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove_from_cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove_product_from_cart/<slug>', remove_single_product_from_cart,
+         name='remove-single-product-from-cart'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('delivery/', DeliveryView.as_view(), name='delivery'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
