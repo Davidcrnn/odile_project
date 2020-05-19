a39b6fd496f63b11697255f87bf6b106eb0f97c4
David Crenin <david.crenin@gmail.com>
2020-05-19 10:12:59 +0200

    test

--- main/templatetags/cart_template_tags.py
+++ main/templatetags/cart_template_tags.py
@@ -4,15 +4,15 @@ from main.models import Order
 register = template.Library()
 
 
-@register.filter
-def cart_product_count(user):
-    if user.is_authenticated:
-        qs = Order.objects.filter(user=user, ordered=False)
-        if qs.exists():
-            total = qs[0].products.count()
-            return total
+# @register.filter
+# def cart_product_count(user):
+#     if user.is_authenticated:
+#         qs = Order.objects.filter(user=user, ordered=False)
+#         if qs.exists():
+#             total = qs[0].products.count()
+#             return total
 
-    return 0
+#     return 0
 
 
 @register.filter
